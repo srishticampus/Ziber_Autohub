a7e9e8f78853038299a9e613f41c1f45c687eb96
Vvk_0105 <vivekbabu.sics@gmail.com>
2025-06-03 16:43:14 +0530

    logout and nevercach added for admin page

--- admin_panel/views.py
+++ admin_panel/views.py
@@ -2,11 +2,17 @@ from django.shortcuts import render, redirect
 from django.contrib.admin.views.decorators import staff_member_required
 from .forms import AddCarForm, AddJobForm
 from hub.models import Car, JobVacancy, PreBooking
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.cache import never_cache
 
+@login_required
+@never_cache
 @staff_member_required
 def admin_dashboard(request):
     return render(request, 'admin_panel/dashboard.html')
 
+@login_required
+@never_cache
 @staff_member_required
 def add_car(request):
     if request.method == 'POST':
@@ -18,6 +24,8 @@ def add_car(request):
         form = AddCarForm()
     return render(request, 'admin_panel/add_car.html', {'form': form})
 
+@login_required
+@never_cache
 @staff_member_required
 def add_job(request):
     if request.method == 'POST':
@@ -29,16 +37,22 @@ def add_job(request):
         form = AddJobForm()
     return render(request, 'admin_panel/add_job.html', {'form': form})
 
+@login_required
+@never_cache
 @staff_member_required
 def car_list(request):
     cars = Car.objects.all()
     return render(request, 'admin_panel/car_list.html', {'cars': cars})
 
+@login_required
+@never_cache
 @staff_member_required
 def job_list(request):
     jobs = JobVacancy.objects.all()
     return render(request, 'admin_panel/job_list.html', {'jobs': jobs})
 
+@login_required
+@never_cache
 @staff_member_required
 def view_prebookings(request):
     bookings = PreBooking.objects.all()
